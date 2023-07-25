206c49929b99a3c09cea6425b278d840498dd25a
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-25 11:06:55 +0330

    add header

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import { Stack } from '@chakra-ui/react'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <Stack h={'60px'} backgroundColor={'orange.400'}>
+        fdsf
+    </Stack>
+  )
+}
+
+export default Header
