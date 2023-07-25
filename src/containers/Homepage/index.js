7174eb129ccc7a78505bba2ae21dcacc9c70c665
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-25 11:06:36 +0330

    add HomePage

---
+++ src/containers/Homepage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+import Menuheader from '../../components/Menuheader'
+const Homepage = () => {
+  return (
+    <div>
+      <Header/>
+      <Menuheader/>
+    </div>
+  )
+}
+
+export default Homepage
