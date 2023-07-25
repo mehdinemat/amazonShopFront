7df754efa147e67437076d57cf9725e694449090
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-25 11:07:15 +0330

    add chakra provider to app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import DataProvider from './redux/store';
+import { ChakraProvider } from '@chakra-ui/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <DataProvider>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </DataProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
