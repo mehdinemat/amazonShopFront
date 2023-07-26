13ca00d12059a1509a2444e9d161c79b690fc080
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-26 12:51:12 +0330

    add product to product_slug

--- src/containers/ProductPageList/index.js
+++ src/containers/ProductPageList/index.js
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout'
 import { useDispatch } from 'react-redux'
 import { getProductSlug } from '../../redux/actions/proudct'
 import { useParams } from 'react-router-dom'
-import { Stack, VStack, Text, Accordion, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel, Box } from '@chakra-ui/react'
+import { Stack, VStack, Text, Accordion, AccordionItem, AccordionButton, AccordionIcon, AccordionPanel, Box, CardBody, Card } from '@chakra-ui/react'
 const ProductPageList = (props) => {
 
   const { slug } = useParams()
@@ -19,9 +19,30 @@ const ProductPageList = (props) => {
     <Layout>
       <VStack>
         <Stack w={'100%'} alignItems={'end'} >
+          <Card w={'100%'} mx={'6px'}>
+            <CardBody>
+              <Accordion defaultIndex={[0]} allowMultiple w={'100%'} py={'5px'} >
+                <AccordionItem>
+                  <h2 style={{ direction: 'rtl' }}>
+                    <AccordionButton>
+                      <Box as="span" flex='1' textAlign='right'>
+                        موبایل ارزان تر از 5 میلیون
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4} textAlign='right'>
+                    این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست
+                  </AccordionPanel>
+                </AccordionItem>
+              </Accordion>
+            </CardBody>
+          </Card>
+        </Stack>
+        <Stack w={'100%'} alignItems={'end'} >
           <Accordion defaultIndex={[0]} allowMultiple w={'100%'} py={'5px'}>
             <AccordionItem>
-              <h2 style={{direction:'rtl'}}>
+              <h2 style={{ direction: 'rtl' }}>
                 <AccordionButton>
                   <Box as="span" flex='1' textAlign='right'>
                     موبایل ارزان تر از 5 میلیون
@@ -30,15 +51,16 @@ const ProductPageList = (props) => {
                 </AccordionButton>
               </h2>
               <AccordionPanel pb={4} textAlign='right'>
-              این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست 
+                این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست
+
               </AccordionPanel>
             </AccordionItem>
           </Accordion>
         </Stack>
         <Stack w={'100%'} alignItems={'end'} >
-        <Accordion defaultIndex={[0]} allowMultiple w={'100%'} py={'5px'}>
+          <Accordion defaultIndex={[0]} allowMultiple w={'100%'} py={'5px'}>
             <AccordionItem>
-              <h2 style={{direction:'rtl'}}>
+              <h2 style={{ direction: 'rtl' }}>
                 <AccordionButton>
                   <Box as="span" flex='1' textAlign='right'>
                     موبایل ارزان تر از 5 میلیون
@@ -47,7 +69,25 @@ const ProductPageList = (props) => {
                 </AccordionButton>
               </h2>
               <AccordionPanel pb={4} textAlign='right'>
-              این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست 
+                این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست
+
+              </AccordionPanel>
+            </AccordionItem>
+          </Accordion>
+        </Stack>
+        <Stack w={'100%'} alignItems={'end'} >
+          <Accordion defaultIndex={[0]} allowMultiple w={'100%'} py={'5px'}>
+            <AccordionItem>
+              <h2 style={{ direction: 'rtl' }}>
+                <AccordionButton>
+                  <Box as="span" flex='1' textAlign='right'>
+                    موبایل ارزان تر از 5 میلیون
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4} textAlign='right'>
+                این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست این برای تست هست
 
               </AccordionPanel>
             </AccordionItem>
