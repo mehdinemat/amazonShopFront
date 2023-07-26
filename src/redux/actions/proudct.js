3944fc5452ad4c8fd8f9dc93f2cf9c470f5ad5ff
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-26 12:51:00 +0330

    add dispatch succes product to action

--- src/redux/actions/proudct.js
+++ src/redux/actions/proudct.js
@@ -8,7 +8,7 @@ export const getProductSlug = ({param}) => async (dispatch) => {
     dispatch({ type: getProductSlugConstants.PRODUCT_REQUEST_SLUG })
     const res = await axiosInstance.get(`/products/${param}`)
 
-    console.log(res.data , 'this is data')
+    dispatch({type:getProductSlugConstants.PRODUCT_SUCCESS_SLUG , payload : {...res.data}})
 
 
   } catch (err) { dispatch({ type: getProductSlugConstants.PRODUCT_FAILURE_SLUG, payload: { error: err.message } }) }
