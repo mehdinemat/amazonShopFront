24f5d609b798c2bfc32d6e7a36a77a370358b98b
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-26 10:57:35 +0330

    add route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Homepage from './containers/Homepage';
-
+import { Routes , BrowserRouter as Router , Route } from 'react-router-dom';
+import ProductPageList from './containers/ProductPageList';
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <Router>
+        <Routes>
+        <Route path='/' extact element={<Homepage/>} />
+        <Route path='/:slug' element={<ProductPageList/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
