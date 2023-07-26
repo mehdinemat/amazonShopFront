c2f0e0b87a1af9902565aa5237562a7a452b537d
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-26 12:08:51 +0330

    get productSlug list with parmas

--- src/containers/ProductPageList/index.js
+++ src/containers/ProductPageList/index.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '../../components/Layout'
+import { useDispatch } from 'react-redux'
+import { getProductSlug } from '../../redux/actions/proudct'
+import { useParams } from 'react-router-dom'
+const ProductPageList = (props) => {
 
-const ProductPageList = () => {
+  const {slug} = useParams()
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    
+    dispatch(getProductSlug({param:slug}))
+  }, [])
+
   return (
     <Layout>
       productapge list
