87d1e1a0be85d4fbb815b6332a774d246090bc6f
mehdinemat <39645561+mehdinemat@users.noreply.github.com>
2023-07-26 10:58:14 +0330

    add Layout productPageList

---
+++ src/containers/ProductPageList/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+
+const ProductPageList = () => {
+  return (
+    <Layout>
+      productapge list
+    </Layout>
+  )
+}
+
+export default ProductPageList
